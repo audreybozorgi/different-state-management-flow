2927ee4f0d2d694caf25e961f6b65861ff7f7104
Mohammad Bozorgi <m.bozorgi@nizek.com>
2023-06-29 21:21:16 +0330

    trees

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import App2 from "./App2";
+import App3 from "./App3";
+import App4 from "./App4";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+const root3 = ReactDOM.createRoot(document.getElementById("root3"));
+const root4 = ReactDOM.createRoot(document.getElementById("root4"));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+root2.render(
+  <React.StrictMode>
+    <App2 />
+  </React.StrictMode>
+);
+root3.render(
+  <React.StrictMode>
+    <App3 />
+  </React.StrictMode>
+);
+root4.render(
+  <React.StrictMode>
+    <App4 />
   </React.StrictMode>
 );
 
