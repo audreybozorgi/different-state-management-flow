a756af93f78d6a072c3f29e51a6f8c88d458b47e
Mohammad Bozorgi <mbozorgi604@gmail.com>
2023-06-29 21:48:52 +0330

    add child components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,69 @@ import  './App.css';
 function App() {
   return (
     <div className='container yellow'>
-      App 1 with props and local state management
-      <Child1Component />
+      <h2>Props and local state management</h2>
+      <h4>Parent</h4>
+      <ChildComponent />
     </div>
   );
 }
 
 export default App;
 
-const Child1Component = () => {
+const ChildComponent = () => {
   return (
-    <div>
-      im child
+    <div 
+      style={{
+        width: '60%', 
+        height: '60%', 
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: 'white'
+        }}
+      >
+      <h4>Child</h4>
+      <GrandChildComponent />
+    </div>
+  )
+}
+
+const GrandChildComponent = () => {
+  return (
+    <div
+    style={{
+      width: '60%', 
+      height: '60%', 
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: 'yellow',
+      padding: '20px',
+      }}
+    >
+      <h4>Grand Child</h4>
+      <DescendantComponent bg={'#83f483'}/>
+      <DescendantComponent bg={'#ff9898'}/>
+    </div>
+  )
+}
+
+const DescendantComponent = ({bg}) => {
+  return (
+    <div
+    style={{
+      width: '60%', 
+      height: '60%', 
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: bg
+      }}
+    >
+      <h4>Descendant Child</h4>
     </div>
   )
 }
